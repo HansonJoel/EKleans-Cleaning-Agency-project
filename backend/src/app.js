4a32959f37db7388159dae3e4aa588d82f94ee8a
HansonJoel <joelhanson002@gmail.com>
2026-06-04 12:58:24 +0100

    Refactor: Reintroduce booking functionality with controller, model, and routes and moved all i.e controllers, routes, model folders into ths src folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const bookingRoutes = require("../routes/bookingRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
 
