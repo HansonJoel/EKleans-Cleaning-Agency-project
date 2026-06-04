7851bf694713ec15375ebc36bc26845517126255
HansonJoel <joelhanson002@gmail.com>
2026-06-04 13:38:51 +0100

    Added Morgan middleware to monitor application logs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
 
 // Middleware
+app.use(morgan("dev"));
 app.use(cors()); // Allow cross-origin requests from your frontend
 app.use(express.json()); // Parse incoming JSON requests
 
